a3017a37b86dbff5648badd0bbf7a09b159c2161
Luke Marsden <me@lukemarsden.net>
2016-10-31 16:53:04 +0000

    Open up frankfurt completely

--- CLOUD_FRANKFURT_AWS/main.tf
+++ CLOUD_FRANKFURT_AWS/main.tf
@@ -99,13 +99,21 @@ resource "aws_security_group" "kubernetes" {
       cidr_blocks = ["0.0.0.0/0"]
   }
 
+  /*
   ingress {
       from_port = 0
       to_port = 0
       protocol = "-1"
       cidr_blocks = ["10.0.0.0/16"]
   }
+  */
 
+  ingress {
+      from_port = 0
+      to_port = 0
+      protocol = "-1"
+      cidr_blocks = ["0.0.0.0/0"]
+  }
 
   egress {
       from_port = 0
