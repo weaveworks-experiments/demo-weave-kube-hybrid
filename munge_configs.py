1d3c259c4b0a62325067ff775e0d691ea987ced2
Luke Marsden <me@lukemarsden.net>
2016-10-31 16:41:55 +0000

    slightly unpleasant hack to workaround tls

--- munge_configs.py
+++ munge_configs.py
@@ -28,6 +28,7 @@ for f in os.listdir("."):
         # clusters
         cluster = kubeconfig["clusters"][0].copy()
         cluster["cluster"]["server"] = "https://%s:%d" % (master_ip, API_PORT)
+        cluster["cluster"]["insecure-skip-tls-verify"] = True
         cluster["name"] = context_name
         output["clusters"].append(cluster)
         # users
